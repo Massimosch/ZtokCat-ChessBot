d72a1da6a129e25930c685c800c21c2bf08c88ff
Minifuse <jani.raitinenn@gmail.com>
2026-02-06 10:15:48 +0200

    Update ruutu.h

--- ruutu.h
+++ ruutu.h
@@ -3,7 +3,7 @@
 // Shakkilaudan ruutu tietyssä (rivi, sarake) -koordinaatissa.
 class Ruutu{
 public:
-	Ruutu(int rivi, int sarake);
+	Ruutu(int sarake, int rivi);
 	Ruutu() {}
 	int getRivi();
 	int getSarake();
